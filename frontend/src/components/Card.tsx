b2b82bfbb4cc4daec38583cac0196a5717147783
TheManishDaksh <140195332+TheManishDaksh@users.noreply.github.com>
2025-05-24 17:56:47 +0530

    some design enhancement

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -7,11 +7,12 @@ type CardType = "twitter" | "youtube" | "doccument";
 interface CardProps{
     type : CardType,
     title : String,
-    link : String,
-    tags : String
+    link? : String,
+    tags : String,
+    text? : String
 }
 
-export default function Card({type, title, link, tags}:CardProps){
+export default function Card({type, title, link, tags, text}:CardProps){
     
     return(
         <div className="p-4">
@@ -30,8 +31,16 @@ export default function Card({type, title, link, tags}:CardProps){
                         <DeleteIcon/>
                     </div>
                </div>
-               <div className="py-3">
+               {/* <div className="py-3">
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas aperiam a quam necessitatibus odit laudantium illum facere. Eveniet officiis, at pariatur dicta sint, atque nostrum labore nemo nam quas incidunt.
+               </div> */}
+               <div>
+                <blockquote data-dnt="true"><a href="https://twitter.com/Reza_Zadeh/status/1344009123004747778?ref_src=twsrc%5Etfw">December 29, 2020</a></blockquote>
+                <script async src="https://platform.twitter.com/widgets.js" charSet="utf-8"></script>
+               </div>
+               
+               <div>
+                <div><iframe src="https://www.youtube.com/embed/q3lX2p_Uy9I?rel=0" className="w-full h-full rounded-lg" allowFullScreen scrolling="no" allow="accelerometer *; clipboard-write *; encrypted-media *; gyroscope *; picture-in-picture *; web-share *;"></iframe></div>
                </div>
                <span className="bg-indigo-200 border-none rounded-full px-2 py-0.5">
                     { tags || "#productivity"}
