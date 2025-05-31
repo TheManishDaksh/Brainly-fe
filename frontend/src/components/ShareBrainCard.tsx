7f113644a091ec5ad4697532419d44b1ee4d6c2a
TheManishDaksh <140195332+TheManishDaksh@users.noreply.github.com>
2025-06-01 00:52:10 +0530

    kuchh na

--- frontend/src/components/ShareBrainCard.tsx
+++ frontend/src/components/ShareBrainCard.tsx
@@ -1,13 +1,18 @@
+import { useState } from "react";
 import Button from "./Button";
 
 export default function ShareBrainCard() {
-  
+  const [ shareLink, setSharLink] = useState('');
     function copyUrl(){
+
+    }
+    function generateLink(){
 
     }
   return (
     <div>
-      <div className=" max-w-96 h-full rounded-lg shadow shadow-slate-400 py-4 px-4">
+      { shareLink.length > 0 ? (
+        <div className=" max-w-96 h-full rounded-lg shadow shadow-slate-400 py-4 px-4">
         <div className="text-lg font-bold"> 
             Please copy the below url
         </div>
@@ -16,7 +21,7 @@ export default function ShareBrainCard() {
             className="w-full min-h-[40px] resize-none rounded-xl border border-slate-300 bg-white px-4 py-3 text-sm shadow-sm outline-none transition-all placeholder-slate-400 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200"
             type="text"
             placeholder="Title"
-            defaultValue={``}
+            defaultValue={shareLink}
             readOnly
           />
         </div>
@@ -25,6 +30,11 @@ export default function ShareBrainCard() {
             onClick={copyUrl}>Copy Url</Button>
         </div>
       </div>
+      ) : (
+        <div className="flex justify-center">
+          <Button className="w-full" variant="default" size="lg" onClick={generateLink}>Genrate Link</Button>
+        </div>
+      )}
     </div>
   );
 }
